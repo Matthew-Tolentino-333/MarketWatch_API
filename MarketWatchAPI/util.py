420e49e9c251adcb7bfdf98ec738b093847781df
matthew-tolentino-333 <matttolent@gmail.com>
2022-02-09 22:08:57 -0800

    Added in trade functionality

--- MarketWatchAPI/util.py
+++ MarketWatchAPI/util.py
@@ -1,52 +1,36 @@
 import requests
-import json
 import re
+import time
+import random
+from bs4 import BeautifulSoup
 
 class MarketClient:
-  def __init__(self):
-    self.session = self.login('matttolent@gmail.com', 'money4me')
+  def __init__(self, username, password):
+    self.session = self.login(username, password)
+    self.headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36'}
   
   def login(self, email, password):
     s = requests.Session()
-    headers = {'Content-Type': 'application/json'}
-    callback_headers = {'content-type': 'application/x-www-form-urlencoded'}
+    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36'}
     payload = {
-      'client_id': '5hssEAdMy0mJTICnJNvC9TXEw3Va7jfO',
-      'connection': 'DJldap',
-      'headers': {
-        'X-REMOTE-USER': email
+      "client_id": "5hssEAdMy0mJTICnJNvC9TXEw3Va7jfO",
+      "connection": "DJldap",
+      "headers": {
+        "X-REMOTE-USER": email
       },
-      'nonce': '86614735-ebb3-4fc2-9674-d0bf0bf7fdc2',
-      'ns': 'prod/accounts-mw',
-      'password': password,
-      'protocol': 'oauth2',
-      'redirect_uri': "https://accounts.marketwatch.com/auth/sso/login",
-      'response_type': "code",
-      'scope': "openid idp_id roles email given_name family_name djid djUsername djStatus trackid tags prts suuid createTimestamp",
-      'state': "hKFo2SBSS2c4UmprX3h0UDdZVEdvbVZldmJTdkhsaERkekxfSqFupWxvZ2luo3RpZNkgWjkyV0JFSHZvb1RhZ0VEbGYxb2Zla09TVk05WFRGVEijY2lk2SA1aHNzRUFkTXkwbUpUSUNuSk52QzlUWEV3M1ZhN2pmTw",
-      'tenant': "sso",
-      'ui_locales': "en-us-x-mw-3-8",
-      'username': email,
-      '_csrf': "sMAl0bPM-NtbjmegNoXqBRiNIQDvAYIWSEZc",
-      '_intstate': "deprecated"
-    }
-
-    callback_payload = {
-      'wa': 'wsignin1.0',
-      'wresult': '',
-      'wctx': {"strategy": "auth0",
-       "auth0Client": "eyJuYW1lIjoiYXV0aDAuanMtdWxwIiwidmVyc2lvbiI6IjkuMTEuMyJ9",
-       "tenant": "sso",
-       "connection": "DJldap",
-       "client_id": "5hssEAdMy0mJTICnJNvC9TXEw3Va7jfO",
-       "response_type": "code",
-       "scope": "openid idp_id roles email given_name family_name djid djUsername djStatus trackid tags prts suuid createTimestamp",
-       "protocol": "oauth2",
-       "redirect_uri": "https://accounts.marketwatch.com/auth/sso/login",
-       "state": "hKFo2SB6T2RwamVOMmtUWTZ2bFFxQ1Z2VG8wWnl3TGQ3X2RXZ6FupWxvZ2luo3RpZNkgX0JZSXJwX1NTQ2FudXVGcVFFeXhlTzBEamN2a2lzMmSjY2lk2SA1aHNzRUFkTXkwbUpUSUNuSk52QzlUWEV3M1ZhN2pmTw",
-       "nonce": "86614735-ebb3-4fc2-9674-d0bf0bf7fdc2",
-       "sid": "I-l8WKkjyP5cp9yXwZ1vB-xMkvExlM_g", # var
-       "realm": "DJldap"}
+      "nonce": "86614735-ebb3-4fc2-9674-d0bf0bf7fdc2",
+      "ns": "prod/accounts-mw",
+      "password": password,
+      "protocol": "oauth2",
+      "redirect_uri": "https://accounts.marketwatch.com/auth/sso/login",
+      "response_type": "code",
+      "scope": "openid idp_id roles email given_name family_name djid djUsername djStatus trackid tags prts suuid createTimestamp",
+      "state": "hKFo2SBSS2c4UmprX3h0UDdZVEdvbVZldmJTdkhsaERkekxfSqFupWxvZ2luo3RpZNkgWjkyV0JFSHZvb1RhZ0VEbGYxb2Zla09TVk05WFRGVEijY2lk2SA1aHNzRUFkTXkwbUpUSUNuSk52QzlUWEV3M1ZhN2pmTw",
+      "tenant": "sso",
+      "ui_locales": "en-us-x-mw-3-8",
+      "username": email,
+      "_csrf": "sMAl0bPM-NtbjmegNoXqBRiNIQDvAYIWSEZc",
+      "_intstate": "deprecated"
     }
 
     # Get state and csrf codes for login session
@@ -57,22 +41,23 @@ class MarketClient:
     payload['_csrf'] = csrf
     payload['state'] = state
 
-    callback_payload['state'] = state
-
     # Attempt login
     try: 
-      r = s.post('https://sso.accounts.dowjones.com/usernamepassword/login', headers=headers, json=payload)
-      # r = s.post('https://sso.accounts.dowjones.com/usernamepassword/login', auth=(email, password))
-      print(r.status_code)
-      # print('--> content', r.text)
-      print('--> headers', r.headers)
-      # print('--> req headers', r.request.headers)
-
-      # handle callback
-      # r = s.get('https://www.marketwatch.com/games/marketbot')
-      # print(r.status_code)
-      # # print('--> content', r.text)
-      # print('--> headers', r.headers)
+      print('try login')
+      response = s.post('https://sso.accounts.dowjones.com/usernamepassword/login', headers=headers, json=payload)
+      hidden_form = BeautifulSoup(response.content, features='lxml')
+      parsed_hidden_form = hidden_form.find_all('input', attrs={'type': 'hidden'})
+      hidden_form_data = {}
+      for input in parsed_hidden_form:
+        hidden_form_data[input['name']] = input['value']
+      s.post('https://sso.accounts.dowjones.com/login/callback', data=hidden_form_data, headers=headers)
+      time.sleep(random.uniform(2,4))
+      res = s.get('https://customercenter.marketwatch.com/home', headers=headers)
+      print(res.url)
+      if res.url == 'https://customercenter.marketwatch.com/home':
+        print('Login Succesful.')
+      else:
+        print('Login Failure.')
 
     except BaseException as e:
       print('Problem with Login')
@@ -82,27 +67,34 @@ class MarketClient:
     # s.headers.update({'authorization': json.loads(r.content)['token']})
     return s
 
-  def order(self):
-    print('---> Ordering...')
+  def trade(self, symbol, amount, tradeType):
+    print('--> Buying...', symbol)
 
-    url = 'https://vse-api.marketwatch.com/v1/games/marketbot2/ledgers/aho-6WQuK9ud/trades'
-    headers = {'Content-Type': 'application/json'}
+    symbol_data = self.getSymbolData(symbol)
+
     payload = {
-        'djid': "13-21983517",
-        'expiresEndOfDay': 'false',
-        'ledgerId': "aho-6WQuK9ud",
-        'orderType': "Market",
-        'shares': 50,
-        'tradeType': "Buy",
+      'djid': symbol_data['djid'],
+      'expiresEndOfDay': 'false',
+      'ledgerId': symbol_data['ledgerId'],
+      'orderType': "Market",
+      'shares': amount,
+      'tradeType': tradeType
     }
-    
-    try:
-      res = self.session.post(url=url, headers=headers, json=payload)
-      print(res.status_code)
-      print('--> content', res.text)
-      print('--> headers', res.headers)
-    except Exception as e:
-      print('Problem with Order')
-      print('Error: {}'.format(e))
-      exit(1)
 
+    res = self.session.post('https://vse-api.marketwatch.com/v1/games/marketbot3/ledgers/{}/trades'.format(symbol_data['ledgerId']), headers=self.headers, json=payload)
+    if res.status_code == 200:
+      print('{} {} of {}'.format(tradeType, amount, symbol))
+
+  # Provide symbol and market identifier code
+  def getSymbolData(self, symbol):
+    charting_symbol_content = self.session.get('https://services.dowjones.com/autocomplete/data?excludeExs=xmstar&featureClass=P&style=full&count=5&need=symbol&q={}&name_startsWith={}'.format(symbol, symbol), headers=self.headers).content
+    charting_symbol = re.search('STOCK\/US\/[A-Z]+\/[A-Z]+', str(charting_symbol_content)).group()
+
+    content = self.session.get("https://www.marketwatch.com/games/marketbot3/tradeorder?chartingSymbol={}".format(charting_symbol, symbol), headers=self.headers).content
+    parsed_data_attrs = BeautifulSoup(content, features='lxml').find('form').attrs  
+    data = {
+      'djid': parsed_data_attrs['data-djkey'],
+      'ledgerId': parsed_data_attrs['data-pub'],
+    }
+
+    return data
--- test.py
+++ test.py
@@ -4,9 +4,14 @@ from MarketWatchAPI.util import MarketClient
 
 def main(args):
   print('Welcome to MarktetWatch bot!')
-  mc_session = MarketClient()
+  mc_session = MarketClient('matttolent@gmail.com', 'money4me')
 
-  mc_session.order()
+  mc_session.trade('AMC', 20, 'Buy')
+  # mc_session.trade('RECAF', 'OOTC', 10, Buy)
+  # mc_session.trade('RECAF', 'OOTC', 10, Sell)
+  # mc_session.trade('RECAF', 'OOTC', 10, Short)
+
+  # mc_session.getSymbolData('AMC')
 
   print('Done testing...')
 
